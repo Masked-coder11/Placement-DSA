b63a11b84cebc4ab9071856383c27cf81a5bcc3f
Masked-coder11 <153896221+Masked-coder11@users.noreply.github.com>
2024-03-10 02:35:21 +0530

    Create Lecture2.cpp

---
+++ Lecture2.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  // // this line is printing my name
+  // // cout<<"Masked"<<endl;
+  // // cout<<"Coder";
+
+  // // cout<<"\n";
+  // // cout<<endl;
+  // // cout<<endl;
+  
+  // cout<<"Masked";
+
+  //talking about inputs
+  // string container;
+  
+  // cin>>container;
+
+  // cout<<container<<" i have got this as input";
+
+  int number=5;
+  cout<<sizeof(number);
+  cout<<endl;
+
+  char alphabet='a';
+  cout<<sizeof(alphabet)<<endl;
+
+  float a=5.5;
+  cout<<sizeof(a)<<endl;
+
+  long long b=1243;
+  cout<<sizeof(b);
+
+  bool flag= true;
+  cout<<sizeof(flag);
+
+  return 0;
+}
